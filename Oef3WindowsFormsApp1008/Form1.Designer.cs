a1d6f998caac448a227d49530dafae0febb104ee
Martijn Wauters <wautersmartijn@gmail.com>
2020-08-10 14:40:06 +0200

    Add project files.

---
+++ Oef3WindowsFormsApp1008/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Oef3WindowsFormsApp1008
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLijst = new System.Windows.Forms.ListBox();
+            this.txtTekst = new System.Windows.Forms.TextBox();
+            this.btnKnop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbLijst
+            // 
+            this.lbLijst.FormattingEnabled = true;
+            this.lbLijst.Location = new System.Drawing.Point(43, 37);
+            this.lbLijst.Name = "lbLijst";
+            this.lbLijst.Size = new System.Drawing.Size(125, 212);
+            this.lbLijst.TabIndex = 0;
+            this.lbLijst.SelectedIndexChanged += new System.EventHandler(this.lbLijst_SelectedIndexChanged);
+            // 
+            // txtTekst
+            // 
+            this.txtTekst.Location = new System.Drawing.Point(43, 272);
+            this.txtTekst.Name = "txtTekst";
+            this.txtTekst.Size = new System.Drawing.Size(125, 20);
+            this.txtTekst.TabIndex = 1;
+            this.txtTekst.TextChanged += new System.EventHandler(this.txtTekst_TextChanged);
+            // 
+            // btnKnop
+            // 
+            this.btnKnop.Location = new System.Drawing.Point(43, 316);
+            this.btnKnop.Name = "btnKnop";
+            this.btnKnop.Size = new System.Drawing.Size(125, 31);
+            this.btnKnop.TabIndex = 2;
+            this.btnKnop.Text = "Filter";
+            this.btnKnop.UseVisualStyleBackColor = true;
+            this.btnKnop.Click += new System.EventHandler(this.btnKnop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKnop);
+            this.Controls.Add(this.txtTekst);
+            this.Controls.Add(this.lbLijst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbLijst;
+        private System.Windows.Forms.TextBox txtTekst;
+        private System.Windows.Forms.Button btnKnop;
+    }
+}
+
---
+++ Oef3WindowsFormsApp1008/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oef3WindowsFormsApp1008
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> NamesList = new List<string>();
+
+        private void lbLijst_SelectedIndexChanged(object sender, EventArgs e)
+
+
+        {
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+            NamesList.Add("Martijn");
+            NamesList.Add("Noah");
+            NamesList.Add("Petra");
+            NamesList.Add("Mokka");
+            NamesList.Add("Willem");
+            NamesList.Add("Marianne");
+            NamesList.Add("Arne");
+            NamesList.Add("Stephanie");
+            NamesList.Add("Vince");
+            NamesList.Add("Bas");
+            NamesList.Add("Jens");
+            NamesList.Add("Kimberley");
+            NamesList.Add("Kobe");
+            NamesList.Add("Lise");
+            NamesList.Add("Kato");
+
+            foreach (var item in NamesList)
+            {
+                lbLijst.Items.Add(item);
+            }
+        }
+
+        private void txtTekst_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKnop_Click(object sender, EventArgs e)
+        {
+            lbLijst.Items.Clear();
+
+            foreach (var item in NamesList)
+            {
+                if (item.ToLower().Contains(txtTekst.Text.ToLower()))
+                {
+                    lbLijst.Items.Add(item);
+                }
+            }
+        }
+    }
+}
